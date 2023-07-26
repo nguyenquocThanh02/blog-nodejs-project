e8141f925bb5efecc54c6a148624c29f742171ec
quocThanh <2002nguyenquocthanh@gmail.com>
2023-07-26 08:55:27 +0700

    push nodejs no mvc

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
 const {engine} = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json()); 
 
 // http logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'));
+
 // template engine
 app.engine('hbs', engine({
   extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources\\views'))
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
+app.get('/search', (req, res) => {
+  // console.log(req.query)
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
