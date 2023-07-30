f9ba934bc14c19a9de2718d7efca613af684a2ef
quocThanh <2002nguyenquocthanh@gmail.com>
2023-07-30 20:38:39 +0700

    complete crud

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db= require('./config/db');
+
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +19,8 @@ app.use(
   }),
 );
 app.use(express.json());
+app.use(methodOverride('_method'));
+
 
 // http logger
 // app.use(morgan('combined'));
@@ -24,14 +30,17 @@ app.engine(
   'hbs',
   engine({
     extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+    }
   }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 // routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const newsRouter = require('./news');
+const meRouter = require('./me');
+const courseRouter = require('./courses');
 const siteRouter = require('./site');
 
 function route(app) {
   app.use('/news', newsRouter);
+  app.use('/me', meRouter);
+  app.use('/courses', courseRouter);
   app.use('/', siteRouter);
 }
 
